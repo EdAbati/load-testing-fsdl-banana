d9bce408b47785dacbece1ac40370da08bf22076
Edoardo Abati <29585319+EdAbati@users.noreply.github.com>
2023-02-05 17:45:54 +0100

    updated locustfile with banana api payload

--- locustfile.py
+++ locustfile.py
@@ -1,14 +1,24 @@
 import json
+import os
 
 from locust import HttpUser, constant, task
 
 IMAGE_URI = "https://fsdl-public-assets.s3-us-west-2.amazonaws.com/paragraphs/a01-077.png"
 
+try:
+    API_KEY = os.environ["API_KEY"]
+except KeyError:
+    raise KeyError("Please set the API_KEY environment variable.")
+try:
+    MODEL_KEY = os.environ["MODEL_KEY"]
+except KeyError:
+    raise KeyError("Please set the MODEL_KEY environment variable.")
+
 
 class TextRecognizerUser(HttpUser):
     wait_time = constant(1)
     headers = {"Content-type": "application/json"}
-    payload = json.dumps({"image_url": IMAGE_URI})
+    payload = json.dumps({"apiKey": API_KEY, "modelKey": MODEL_KEY, "modelInputs": {"image": IMAGE_URI}})
 
     @task
     def predict(self):
